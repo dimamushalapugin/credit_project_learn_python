66a12a09057ec8d16321edec717cae031d4e3da4
БадриевА <zzdl4@bk.ru>
2023-06-16 14:32:14 +0300

    1. Закинул templates в папку webapp 2. Добавил POST в функуцию load_xlsx (нужно еще доделать, пока что добавляется в базу данных только дата)

--- webapp/credit_app.py
+++ webapp/credit_app.py
@@ -1,5 +1,6 @@
 import models
 from flask import Flask, render_template, redirect, url_for, request
+from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -39,19 +40,35 @@ def list_of_user():
 
 @app.route('/delete_user/<int:user_id>', methods=['GET', 'POST'])
 def delete_user(user_id):
-    user = models.User.query.get(user_id)
+    user = models.db.session.get(models.User, user_id)
     if user:
         user.delete()
     return redirect(url_for('list_of_user'))
 
 
-@app.route('/first_page')
+@app.route('/first_page', methods=['GET', 'POST'])
 def load_xlsx():
+    if request.method == 'POST':
+        leasing_contract = request.form['leasing_contract']
+        payment_date_str = request.form['payment_date']
+        payment_date = datetime.strptime(payment_date_str, '%d.%m.%Y').date()
+        credit_contract = request.form['credit_contract']
+
+        new_payment = models.Payment(payment_date=payment_date)
+        models.db.session.add(new_payment)
+        models.db.session.commit()
+        return redirect(url_for('success'))
+
     return render_template('first_page.html')
 
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
+    return render_template('credit_table_page.html')
+
+
+@app.route('/success', methods=['POST', 'GET'])
+def success():
     return render_template('credit_table_page.html')
 
 
