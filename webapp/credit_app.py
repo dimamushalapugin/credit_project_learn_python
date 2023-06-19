c2f731ad0046241dfa4bae0eb3a8afa7b2fdeebe
БадриевА <zzdl4@bk.ru>
2023-06-19 17:34:38 +0300

    1. Изменил нейинг 2. Полностью заполняется таблица Payments по странице "Загрузка графиков по кредитам" 3. Загружается график в базу данных (осатется доделать, чтобы дата платежа приходилась на раб. день)

--- webapp/credit_app.py
+++ webapp/credit_app.py
@@ -1,6 +1,7 @@
 import models
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 from datetime import datetime
+import pandas as pd
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -27,13 +28,13 @@ def create_user():
         models.db.session.add(new_user)
         models.db.session.commit()
 
-        return redirect(url_for('list_of_user'))
+        return redirect(url_for('list_of_users'))
 
     return render_template('create_user_page.html')
 
 
 @app.route('/list_user')
-def list_of_user():
+def list_of_users():
     users = models.User.query.all()
     return render_template('users_list.html', users=users)
 
@@ -43,7 +44,7 @@ def delete_user(user_id):
     user = models.db.session.get(models.User, user_id)
     if user:
         user.delete()
-    return redirect(url_for('list_of_user'))
+    return redirect(url_for('list_of_users'))
 
 
 def write_to_db(new_data):
@@ -60,33 +61,46 @@ def assign_leasing_contract_id(request_form):
 def find_credit_contract_id(request_form):
     new_credit_contract = models.CreditContract.query.filter(
         models.CreditContract.credit_contract_name == request_form).first()
-    print(request_form)
     if new_credit_contract:
         return new_credit_contract.id
     return None
 
 
+@app.route('/total_amount_from_xlsx', methods=['POST'])
+def total_amount_from_xlsx():
+    df = pd.read_excel(request.files['uploaded_file'])
+    return str(df['amount'].sum())
+
+
 @app.route('/first_page', methods=['GET', 'POST'])
-def load_xlsx():
+def create_payment():
     if request.method == 'POST':
         leasing_contract = assign_leasing_contract_id(request.form['leasing_contract'])
         payment_date = datetime.strptime(request.form['payment_date'], '%d.%m.%Y').date()
         credit_contract = find_credit_contract_id(request.form['credit_contract'])
+        amount = float(total_amount_from_xlsx())
 
         new_payment = models.Payment(payment_date=payment_date, leasing_contract_id=leasing_contract,
-                                     credit_contract_id=credit_contract)
+                                     credit_contract_id=credit_contract, amount=amount)
         write_to_db(new_payment)
-        return redirect(url_for('success'))
+        create_payment_schedule(new_payment)
+
+        return redirect(url_for('home'))
     return render_template('first_page.html')
+
+
+def create_payment_schedule(new_payment):
+    df = pd.read_excel(request.files['uploaded_file'])
+    for index, row in df.iterrows():
+        new_payment_schedule = models.PaymentSchedule(payment_id=new_payment.id, payment_date=row['payment_date'],
+                                                      amount=row['amount'], interest_rate=row['interest_rate'])
+        models.db.session.add(new_payment_schedule)
+    models.db.session.commit()
+    return jsonify(message='Файл успешно загружен')
 
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
-    return render_template('credit_table_page.html')
-
-
-@app.route('/success', methods=['POST', 'GET'])
-def success():
     return render_template('credit_table_page.html')
 
 
