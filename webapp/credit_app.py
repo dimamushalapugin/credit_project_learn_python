acaeb62dd5c22aebaa43378d90019c8ab3a3cada
БадриевА <zzdl4@bk.ru>
2023-07-01 20:24:49 +0300

    1. Добавили авторизацию пользователя (admin или нет) 2. Добавили хэширование паролей

--- webapp/credit_app.py
+++ webapp/credit_app.py
@@ -4,33 +4,41 @@ from flask import Flask, render_template, redirect, url_for, request, flash, jso
 from datetime import datetime
 from sql_queries import write_to_db, assign_leasing_contract_id, find_credit_contract_id, create_payment_schedule, \
     query_for_all_payments, query_for_daily_payments, query_for_bank_debts
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
+from webapp.forms import LoginForm
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 models.db.init_app(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-@app.route('/login', methods=['POST', 'GET'])
+
+@login_manager.user_loader
+def load_user(user_id):
+    return models.User.query.get(user_id)
+
+
+@app.route('/login')
 def login():
-    if request.method == 'POST':
-        username = request.form['inputEmail']
-        password = request.form['inputPassword']
-        try:
-            user = models.User.query.filter(models.User.login == username).first().login
-        except AttributeError:
-            user = None
-        try:
-            password_check = models.User.query.filter(models.User.password == password).first().password
-        except AttributeError:
-            password_check = None
-        if user == username and password_check == password:
+    title = "Авторизация"
+    login_form = LoginForm()
+    return render_template('login_page.html', page_title=title, form=login_form)
+
+
+@app.route('/process-login', methods=['POST'])
+def process_login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = models.User.query.filter_by(login=form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            flash('Вы вошли на сайт')
             return redirect(url_for('list_of_all_payments'))
-        elif user != username or password_check != password:
-            flash('Неверный логин или пароль', 'error')
-            return redirect(url_for('login'))
-        else:
-            return redirect(url_for('login'))
-    return render_template('login_page.html')
+    flash('Неправильное имя пользователя или пароль')
+    return redirect(url_for('login'))
 
 
 @app.route('/full_credit_info/<path:leasing_contract_number>', methods=['GET', 'POST'])
@@ -43,32 +51,47 @@ def full_credit_info(leasing_contract_number):
 
 @app.route('/logout')
 def exit_user():
+    logout_user()
     return redirect(url_for('login'))
 
 
 @app.route('/create_new_user', methods=['GET', 'POST'])
+@login_required
 def create_user():
-    if request.method == 'POST':
-        email = request.form['type_pl']
-        password = request.form['identificator_pl']
-        new_user = models.User(login=email, password=password, blocked=False)
-        write_to_db(new_user)
-        return redirect(url_for('list_of_users'))
-    return render_template('create_user_page.html')
+    if current_user.is_admin:
+        if request.method == 'POST':
+            login = request.form['type_pl']
+            new_user = models.User(login=login, blocked=False, role='admin')
+            new_user.set_password(request.form['identificator_pl'])
+
+            if models.User.query.filter(models.User.login == login).count():
+                flash('Такой пользователь уже есть')
+                return redirect(url_for('create_user'))
+            write_to_db(new_user)
+            return redirect(url_for('list_of_users'))
+        return render_template('create_user_page.html')
+    else:
+        return redirect(url_for('list_of_all_payments'))
 
 
 @app.route('/list_user')
+@login_required
 def list_of_users():
-    users = models.User.query.all()
-    return render_template('users_list.html', users=users)
+    if current_user.is_admin:
+        users = models.User.query.all()
+        return render_template('users_list.html', users=users)
+    else:
+        return redirect(url_for('list_of_all_payments'))
 
 
 @app.route('/credit_table')
+@login_required
 def list_of_all_payments():
     return render_template('credit_table_page.html', result=query_for_all_payments())
 
 
 @app.route('/daily_payments')
+@login_required
 def list_of_daily_payments():
     today_str = datetime.now().strftime('%d.%m.%Y')
     return render_template('daily_payments.html', result=query_for_daily_payments(), bank_debts=query_for_bank_debts(),
@@ -90,22 +113,27 @@ def total_amount_from_xlsx():
 
 
 @app.route('/first_page', methods=['GET', 'POST'])
+@login_required
 def create_payment():
-    if request.method == 'POST':
-        leasing_contract = assign_leasing_contract_id(request.form['leasing_contract'])
-        date_of_issue = datetime.strptime(request.form['date_of_issue'], '%Y-%m-%d').date()
-        credit_contract = find_credit_contract_id(request.form['credit_contract'])
-        total_amount = float(total_amount_from_xlsx())
+    if current_user.is_admin:
+        if request.method == 'POST':
+            leasing_contract = assign_leasing_contract_id(request.form['leasing_contract'])
+            date_of_issue = datetime.strptime(request.form['date_of_issue'], '%Y-%m-%d').date()
+            credit_contract = find_credit_contract_id(request.form['credit_contract'])
+            total_amount = float(total_amount_from_xlsx())
 
-        new_payment = models.Payment(date_of_issue=date_of_issue, leasing_contract_id=leasing_contract,
-                                     credit_contract_id=credit_contract, total_amount=total_amount)
-        write_to_db(new_payment)
-        create_payment_schedule(new_payment)
+            new_payment = models.Payment(date_of_issue=date_of_issue, leasing_contract_id=leasing_contract,
+                                         credit_contract_id=credit_contract, total_amount=total_amount)
+            write_to_db(new_payment)
+            create_payment_schedule(new_payment)
+            return redirect(url_for('list_of_all_payments'))
+        return render_template('first_page.html')
+    else:
         return redirect(url_for('list_of_all_payments'))
-    return render_template('first_page.html')
 
 
 @app.route('/')
+@login_required
 def home():
     return redirect(url_for('list_of_all_payments'))
 
