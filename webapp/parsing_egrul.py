5061859d10617b4fee0a93c3071266addd5610a8
БадриевА <zzdl4@bk.ru>
2023-07-07 12:47:14 +0300

    .upper()

--- webapp/parsing_egrul.py
+++ webapp/parsing_egrul.py
@@ -15,16 +15,17 @@ def get_customer_name(request_form):
     info_about_customer = parsing_egrul_json(request_form)
     if 'СвЮЛ' in info_about_customer:
         try:
-            info_about_customer = info_about_customer['СвЮЛ']['СвНаимЮЛ']['СвНаимЮЛСокр']['@attributes']['НаимСокр']
+            info_about_customer = info_about_customer['СвЮЛ']['СвНаимЮЛ']['СвНаимЮЛСокр']['@attributes'][
+                'НаимСокр'].upper()
         except KeyError:
             info_about_customer = None
     elif 'СвИП' in info_about_customer:
         info_about_customer = info_about_customer.get('СвИП')
         if info_about_customer:
             fio_rus = info_about_customer.get('СвФЛ', {}).get('ФИОРус', {}).get('@attributes', {})
-            first_name = fio_rus.get('Имя', '-')
-            last_name = fio_rus.get('Фамилия', '-')
-            patronymic = fio_rus.get('Отчество', '-')
+            first_name = fio_rus.get('Имя', '-').upper()
+            last_name = fio_rus.get('Фамилия', '-').upper()
+            patronymic = fio_rus.get('Отчество', '-').upper()
             info_about_customer = f'{last_name} {first_name} {patronymic}'
         else:
             info_about_customer = None
@@ -32,4 +33,3 @@ def get_customer_name(request_form):
         info_about_customer = None
 
     return info_about_customer
-
