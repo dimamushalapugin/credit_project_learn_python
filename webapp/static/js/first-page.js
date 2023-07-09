3b010b88b75c6bd7d8520fd6ea49fb78f9ea83e8
badrievad <zzdl4@bk.ru>
2023-07-09 17:27:56 +0300

    Добавил создание админа и заполнение таблицы Банки и Крежитные договоры

--- webapp/static/js/first-page.js
+++ webapp/static/js/first-page.js
@@ -4,55 +4,36 @@ function updateCreditContracts() {
 
                     contractSelect.innerHTML = "";
 
-                    if (investorSelect.value === "mkb") {
-                        contractSelect.innerHTML += '<option value="МКБ1">МКБ1</option>';
-                        contractSelect.innerHTML += '<option value="МКБ2">МКБ2</option>';
-                        contractSelect.innerHTML += '<option value="МКБ3">МКБ3</option>';
-                        contractSelect.innerHTML += '<option value="МКБ5">МКБ5</option>';
+                    if (investorSelect.value === "bank1") {
+                        contractSelect.innerHTML += '<option value="Банк1 Договор1">Банк1 Договор1</option>';
+                        contractSelect.innerHTML += '<option value="Банк1 Договор2">Банк1 Договор2</option>';
                     }
-                    else if (investorSelect.value === "abb") {
-                        contractSelect.innerHTML += '<option value="АББ4">АББ4</option>';
-                        contractSelect.innerHTML += '<option value="АББ5">АББ5</option>';
-                        contractSelect.innerHTML += '<option value="АББ6">АББ6</option>';
-                        contractSelect.innerHTML += '<option value="АББ7">АББ7</option>';
-                        contractSelect.innerHTML += '<option value="АББ8">АББ8</option>';
-                        contractSelect.innerHTML += '<option value="АББ9">АББ9</option>';
+                    else if (investorSelect.value === "bank2") {
+                        contractSelect.innerHTML += '<option value="Банк2 Договор1">Банк2 Договор1</option>';
+                        contractSelect.innerHTML += '<option value="Банк2 Договор2">Банк2 Договор2</option>';
                     }
-                     else if (investorSelect.value === "mib") {
-                        contractSelect.innerHTML += '<option value="МИБ1">МИБ1</option>';
-                        contractSelect.innerHTML += '<option value="МИБ2">МИБ2</option>';
-                        contractSelect.innerHTML += '<option value="МИБ3">МИБ3</option>';
-                        contractSelect.innerHTML += '<option value="МИБ4">МИБ4</option>';
+                     else if (investorSelect.value === "bank3") {
+                        contractSelect.innerHTML += '<option value="Банк3 Договор1">Банк3 Договор1</option>';
+                        contractSelect.innerHTML += '<option value="Банк3 Договор2">Банк3 Договор2</option>';
+                        contractSelect.innerHTML += '<option value="Банк3 Договор3">Банк3 Договор3</option>';
                     }
-                    else if (investorSelect.value === "alfa") {
-                        contractSelect.innerHTML += '<option value="Альфа1">Альфа1</option>';
+                    else if (investorSelect.value === "bank4") {
+                        contractSelect.innerHTML += '<option value="Банк4 Договор1">Банк4 Договор1</option>';
                     }
-                    else if (investorSelect.value === "pub") {
-                        contractSelect.innerHTML += '<option value="ПУБ1">ПУБ1</option>';
-                        contractSelect.innerHTML += '<option value="ПУБ2">ПУБ2</option>';
-                        contractSelect.innerHTML += '<option value="ПУБ3">ПУБ3</option>';
-                        contractSelect.innerHTML += '<option value="ПУБ4">ПУБ4</option>';
-                        contractSelect.innerHTML += '<option value="ПУБ5">ПУБ5</option>';
-                        contractSelect.innerHTML += '<option value="ПУБ6">ПУБ6</option>';
-                        contractSelect.innerHTML += '<option value="ПУБ7">ПУБ7</option>';
-                        contractSelect.innerHTML += '<option value="ПУБ8">ПУБ8</option>';
+                    else if (investorSelect.value === "bank5") {
+                        contractSelect.innerHTML += '<option value="Банк5 Договор1">Банк5 Договор1</option>';
+                        contractSelect.innerHTML += '<option value="Банк5 Договор2">Банк5 Договор2</option>';
                     }
-                    else if (investorSelect.value === "ural_fd") {
-                        contractSelect.innerHTML += '<option value="Урал1">Урал1</option>';
-                        contractSelect.innerHTML += '<option value="Урал2">Урал2</option>';
-                        contractSelect.innerHTML += '<option value="Урал3">Урал3</option>';
+                    else if (investorSelect.value === "bank6") {
+                        contractSelect.innerHTML += '<option value="Банк6 Договор1">Банк6 Договор1</option>';
                     }
-                    else if (investorSelect.value === "keb") {
-                        contractSelect.innerHTML += '<option value="КЕБ1">КЕБ1</option>';
-                        contractSelect.innerHTML += '<option value="КЕБ2">КЕБ2</option>';
+                    else if (investorSelect.value === "bank7") {
+                        contractSelect.innerHTML += '<option value="Банк7 Договор1">Банк7 Договор1</option>';
                     }
-                    else if (investorSelect.value === "solid") {
-                        contractSelect.innerHTML += '<option value="СОЛ1">СОЛ1</option>';
-                        contractSelect.innerHTML += '<option value="СОЛ2">СОЛ2</option>';
-                        contractSelect.innerHTML += '<option value="СОЛ3">СОЛ3</option>';
+                    else if (investorSelect.value === "bank8") {
+                        contractSelect.innerHTML += '<option value="Банк8 Договор1">Банк8 Договор1</option>';
                     }
                 }
-
 
                 window.onload = function() {
                     updateCreditContracts();
