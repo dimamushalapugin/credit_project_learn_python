3a03ce66885458808b933fe55cab8934e8819950
badrievad <zzdl4@bk.ru>
2023-07-22 09:59:47 +0300

    role

--- webapp/user/create_admin.py
+++ webapp/user/create_admin.py
@@ -4,7 +4,7 @@ from webapp import config
 
 app = create_app()
 with app.app_context():
-    user = User(login=config.LOGIN, blocked=False, role='admin')
+    user = User(login=config.LOGIN, blocked=False, role='user')
     user.set_password(config.PASSWORD)
     db.session.add(user)
     db.session.commit()
