a7b59d5bb7c6e0730f6789977d8fc7e98030670f
badrievad <zzdl4@bk.ru>
2023-08-13 16:39:19 +0300

    update папки сохранения риск заключений

--- webapp/risk/views.py
+++ webapp/risk/views.py
@@ -2,28 +2,50 @@ import time
 
 import openpyxl
 import os
-from flask import Blueprint, flash, render_template, redirect, request, url_for, send_from_directory
+from flask import Blueprint, flash, render_template, redirect, request, url_for, send_from_directory, jsonify, Response
 from webapp.user.auth_utils import admin_required
 
 blueprint = Blueprint('risk', __name__, url_prefix='/risk')
+
+
+def get_folder_names(folder_path):
+    folder_names = []
+    absolute_folder_path = os.path.join('webapp', folder_path)
+    if os.path.exists(absolute_folder_path) and os.path.isdir(absolute_folder_path):
+        folder_names = [item for item in os.listdir(absolute_folder_path) if
+                        os.path.isdir(os.path.join(absolute_folder_path, item))]
+    return folder_names
 
 
 @blueprint.route('/risk_conclusion')
 @admin_required
 def risk_page():
     print('__risk_page__')
-    filenames = os.listdir('webapp/static/files')
-    return render_template('risk_conclusion.html', filenames=filenames)
+    folder_path = 'static/files'
+    folder_names = get_folder_names(folder_path)  # Функция для получения списка папок
+    print(folder_names)
+    return render_template('risk_conclusion.html', folder_names=folder_names)
 
 
 @blueprint.route('/risk_conclusion/<path:folder_path>')
-def risk_conclusion_folder(folder_path, filename):
-    folder_path = os.path.join('webapp/static/files', folder_path)
-    if os.path.exists(folder_path) and os.path.isdir(folder_path):
-        items = os.listdir(folder_path)
-        return render_template('risk_conclusion_folder.html', items=items)
-    else:
-        return "Folder not found", 404
+def risk_conclusion_folder(folder_path):
+    print('__risk_conclusion_folder__')
+    # Сформировать абсолютный путь к папке на основе базовой директории
+    base_folder = 'webapp/static/files'
+    absolute_folder_path = os.path.join(base_folder, folder_path).replace('\\', '/')
+
+    contents = []
+
+    # Проверить, существует ли папка
+    if os.path.exists(absolute_folder_path) and os.path.isdir(absolute_folder_path):
+        items = os.listdir(absolute_folder_path)
+
+        # Преобразовать список элементов в список словарей для передачи в шаблон
+        contents = [{"name": item, "path": os.path.join(folder_path, item).replace('\\', '/')} for item in items]
+    print(contents)
+    print(folder_path)
+    print(absolute_folder_path)
+    return render_template('risk_conclusion_folder.html', folder_path=folder_path, items=contents)
 
 
 def create_xlsx_file(data):
@@ -53,6 +75,11 @@ def create_xlsx():
         return redirect(url_for('risk.risk_page'))
 
 
-@blueprint.route('/download/<path:filename>')
+@blueprint.route('/download/<filename>')
 def download(filename):
-    return send_from_directory(f'static/files/', filename, as_attachment=True)
+    folder_path = request.args.get('folder_path')
+    print('__Download__')
+    print(filename)
+    print(folder_path)
+    real_path = os.path.join('static', 'files', folder_path).replace('\\', '/').replace(f"/{filename}", '')
+    return send_from_directory(real_path, filename, as_attachment=True)
