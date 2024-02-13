24640a8bd29259c163db28d02a2ca76377c2a7bd
БадриевА <zzdl4@bk.ru>
2024-02-13 17:04:56 +0300

    update на xlrd и xlutils

--- webapp/managers/merge_two_xlsx.py
+++ webapp/managers/merge_two_xlsx.py
@@ -1,7 +1,8 @@
 import os
-import xlwings as xw
+import xlrd
 
 from webapp.config import ABS_PATH_APP, ABS_PATH_TEMP
+from xlutils.copy import copy as xl_copy
 
 
 def merge_files(file_name, download_name):
@@ -9,23 +10,21 @@ def merge_files(file_name, download_name):
     file_path2 = os.path.join(ABS_PATH_APP, 'ШАБЛОН ЗАЯВКИ ЛКМБ.xlsx')
     merged_file_path = os.path.join(ABS_PATH_TEMP, download_name)
 
-    # Подключение к серверу xlwings
-    app = xw.App(impl="openpyxl", visible=False)
-
     try:
-        # Открытие книг Excel
-        workbook1 = app.books.open(file_path1)
-        workbook2 = app.books.open(file_path2)
+        # Открытие первого файла для чтения
+        workbook1 = xlrd.open_workbook(file_path1, formatting_info=True)
+        workbook2 = xlrd.open_workbook(file_path2, formatting_info=True)
 
-        # Копирование листов
-        for sheet in workbook1.sheets:
-            sheet.api.Copy(Before=workbook2.sheets[0].api)
+        # Копирование листа из первого файла во второй
+        wb1_sheet = workbook1.sheet_by_index(0)
+        wb2 = xl_copy(workbook2)
+        wb1 = wb2.add_sheet('Sheet1', cell_overwrite_ok=True)
+        for row in range(wb1_sheet.nrows):
+            for col in range(wb1_sheet.ncols):
+                wb1.write(row, col, wb1_sheet.cell_value(row, col))
 
-        # Сохранение и закрытие
-        workbook2.save(merged_file_path)
-        workbook2.close()
-        workbook1.close()
+        # Сохранение изменений во втором файле
+        wb2.save(merged_file_path)
 
-    finally:
-        # Закрытие приложения
-        app.quit()
+    except Exception as e:
+        print(f"Ошибка при объединении файлов: {e}")
