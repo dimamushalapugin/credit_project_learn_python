2fbf1d7c2a5ebb2fead753b59fd220bac3f85215
БадриевА <zzdl4@bk.ru>
2024-02-14 09:11:25 +0300

    update

--- webapp/managers/merge_two_xlsx.py
+++ webapp/managers/merge_two_xlsx.py
@@ -1,33 +1,25 @@
 import os
-from openpyxl import load_workbook
-from openpyxl import Workbook
+import xlwings as xw
 
 from webapp.config import ABS_PATH_APP, ABS_PATH_TEMP
 
 
 def merge_files(file_name, download_name):
+    # Получить полные пути к файлам
     file_path1 = os.path.join(ABS_PATH_TEMP, file_name)
     file_path2 = os.path.join(ABS_PATH_APP, 'ШАБЛОН ЗАЯВКИ ЛКМБ.xlsx')
     merged_file_path = os.path.join(ABS_PATH_TEMP, download_name)
 
-    try:
-        # Загрузка первого файла для чтения
-        workbook1 = load_workbook(filename=file_path1)
-        # Загрузка второго файла (если он существует) или создание нового
-        try:
-            workbook2 = load_workbook(filename=file_path2)
-        except FileNotFoundError:
-            workbook2 = Workbook()
+    # Открыть первую и вторую книги Excel
+    workbook1 = xw.Book(file_path1)
+    workbook2 = xw.Book(file_path2)
 
-        # Получение листа из первого файла
-        sheet1 = workbook1.active
-        # Копирование листа во второй файл
-        sheet2 = workbook2.copy_worksheet(sheet1)
-        # Переименование скопированного листа (если требуется)
-        sheet2.title = 'Sheet1'
+    # Копировать листы из первой книги во вторую
+    for sheet in workbook1.sheets:
+        sheet.api.Copy(Before=workbook2.sheets[0].api)
 
-        # Сохранение изменений во втором файле
-        workbook2.save(merged_file_path)
+    # Сохранить изменения во второй книге
+    workbook2.save(merged_file_path)
+    workbook2.close()
+    workbook1.close()
 
-    except Exception as e:
-        print(f"Ошибка при объединении файлов: {e}")
