f984bb9a078f27906911322ec6fae54af5f03250
БадриевА <zzdl4@bk.ru>
2024-02-20 16:54:42 +0300

    update добавлено поле для физ. лица (риск)

--- webapp/risk/views.py
+++ webapp/risk/views.py
@@ -7,7 +7,7 @@ from flask import Blueprint, flash, render_template, redirect, request, url_for,
 from flask_login import current_user
 from webapp.config import DADATA_TOKEN_BKI
 from webapp.user.auth_utils import risk_required
-from webapp.risk.new_create_risk_conclusion import create_conclusion
+from webapp.risk.new_create_risk_conclusion import create_conclusion, create_conclusion_individual
 from webapp.risk.logger import logging
 from webapp.risk.mongo_db import MongoDB
 
@@ -25,13 +25,28 @@ def get_folder_names(folder_path):
     return folder_names
 
 
+def get_folder_names_individual(folder_path):
+    individual_folder_names = []
+    absolute_folder_path = os.path.join('webapp', folder_path)
+    if os.path.exists(absolute_folder_path) and os.path.isdir(absolute_folder_path):
+        individual_folder_names = [item for item in os.listdir(absolute_folder_path) if
+                                   os.path.isdir(os.path.join(absolute_folder_path, item))]
+        individual_folder_names.sort(key=lambda x: os.path.getmtime(os.path.join(absolute_folder_path, x)),
+                                     reverse=True)
+
+    return individual_folder_names
+
+
 @blueprint.route('/risk_conclusion')
 @risk_required
 def risk_page():
     folder_path = Path('static') / 'files'
+    folder_path_individual = Path('static') / 'files-individual'
     folder_names = get_folder_names(folder_path)  # Функция для получения списка папок
+    individual_folder_names = get_folder_names_individual(folder_path_individual)  # Функция для получения списка папок физ. лиц
     suggestions_token = DADATA_TOKEN_BKI
     return render_template('risk_conclusion.html', folder_names=folder_names,
+                           individual_folder_names=individual_folder_names,
                            suggestions_token=suggestions_token)
 
 
@@ -63,16 +78,25 @@ def create_xlsx_file(data):
         return False
 
 
+def create_xlsx_file_individual(data):
+    try:
+        create_conclusion_individual(data['client_inn'])
+        return True
+    except Exception:
+        flash('Вас выбили из Дельты/Ошибка', 'info')
+        raise ValueError('Ошибка')
+
+
 @blueprint.route('/create_xlsx', methods=['POST'])
 def create_risk_conclusion():
-    logging.info(f"{current_user} Нажал на кнопку 'Создать риск-заключение'")
+    logging.info(f"{current_user} - Нажал на кнопку 'Создать риск-заключение'")
     start_time = time.perf_counter()
     try:
         data = request.form
         file_name = create_xlsx_file(data)
         end_time = time.perf_counter()
         execution_time = end_time - start_time
-        logging.info(f"({current_user}) Риск-заключение создалось за: {execution_time:.1f} сек.")
+        logging.info(f"({current_user}) - Риск-заключение создалось за: {execution_time:.1f} сек.")
         if file_name:
             flash(f'Файл успешно создан', 'success')
             mongo = MongoDB(current_user)
@@ -80,6 +104,26 @@ def create_risk_conclusion():
             return redirect(url_for('risk.risk_page', file_name=file_name))
         else:
             return redirect(url_for('risk.risk_page', file_name=file_name))
+    except Exception as e:
+        flash(str(e), 'error')
+        return redirect(url_for('risk.risk_page'))
+
+
+@blueprint.route('/create_xlsx_individual', methods=['POST'])
+def create_risk_conclusion_individual():
+    logging.info(f"{current_user}  - Нажал на кнопку 'Проверка физ. лица'")
+    start_time = time.perf_counter()
+    try:
+        data = request.form
+        file_name_individual = create_xlsx_file_individual(data)
+        end_time = time.perf_counter()
+        execution_time = end_time - start_time
+        logging.info(f"({current_user}) Риск-заключение создалось за: {execution_time:.1f} сек.")
+        if file_name_individual:
+            flash(f'Файл успешно создан', 'success')
+            return redirect(url_for('risk.risk_page', file_name_individual=file_name_individual))
+        else:
+            return redirect(url_for('risk.risk_page', file_name_individual=file_name_individual))
     except Exception as e:
         flash(str(e), 'error')
         return redirect(url_for('risk.risk_page'))
