6ceae21c31f9bd963051b4a2df6894cde8591dcb
БадриевА <zzdl4@bk.ru>
2024-02-24 11:10:55 +0300

    added loader

--- webapp/static/js/risk_concl_loader.js
+++ webapp/static/js/risk_concl_loader.js
@@ -1,7 +1,11 @@
 // Получаем ссылки на кнопки и инпуты для файлов
     var submitButton = document.getElementById("submit-button");
+    var submitButton_individual = document.getElementById("submit-button-individual");
     var clientInnInput = document.getElementById('client_inn_form');
     var sellerInnInput = document.getElementById('seller_inn_form');
+    var individualInn = document.getElementById('individual_inn');
+    var individualName = document.getElementById('name');
+    var individualSurname = document.getElementById('surname');
 
     document.addEventListener('DOMContentLoaded', function () {
     var loadingIndicator = document.getElementById('loading-indicator');
@@ -9,6 +13,24 @@
     submitButton.addEventListener('click', function () {
 
         if (clientInnInput.value.trim() === '' || sellerInnInput.value.trim() === '') {
+            return;
+        }
+
+        // Показываем индикатор загрузки
+        loadingIndicator.style.display = 'block';
+
+        // Здесь можно добавить код для отправки формы или другие действия при нажатии на кнопку
+        // Например, использовать AJAX для отправки данных на сервер и дождаться ответа
+
+        // В данном примере, просто устанавливаем событие, чтобы скрыть индикатор после полной загрузки страницы
+        window.addEventListener('load', function () {
+            // Скрываем индикатор загрузки
+            loadingIndicator.style.display = 'none';
+        });
+    });
+    submitButton_individual.addEventListener('click', function () {
+
+        if (individualInn.value.trim() === '' || individualName.value.trim() === '' || individualSurname.value.trim() === '') {
             return;
         }
 
