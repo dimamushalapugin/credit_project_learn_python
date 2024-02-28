771827d2915c8939d7ad07ee545e03b5f707dbdf
БадриевА <zzdl4@bk.ru>
2024-02-28 12:52:43 +0300

    update logger

--- webapp/risk/logger.py
+++ webapp/risk/logger.py
@@ -6,13 +6,17 @@ logger = logging.getLogger(__name__)
 file_log = logging.FileHandler(PATH_FOR_LOG)
 console_out = logging.StreamHandler()
 
-logging.basicConfig(handlers=(file_log, console_out), level=logging.INFO,
-                    format="%(asctime)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s",
-                    datefmt='%d.%m.%Y %H:%M:%S')
+logging.basicConfig(
+    handlers=(file_log, console_out),
+    level=logging.INFO,
+    format="%(asctime)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s",
+    datefmt="%d.%m.%Y %H:%M:%S",
+)
 
 formatter = logging.Formatter(
     "%(asctime)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s",
-    datefmt='%H:%M:%S')
+    datefmt="%d.%m.%Y %H:%M:%S",
+)
 console_out.setFormatter(formatter)
 
 logger.addHandler(console_out)
