9b09b3250003c7abba7bc1131aadcbe00e597c6d
БадриевА <zzdl4@bk.ru>
2024-03-10 12:09:40 +0300

    update

--- webapp/statistics/sql_queries_expert.py
+++ webapp/statistics/sql_queries_expert.py
@@ -47,6 +47,64 @@ class ExpertRa:
             )
         return total_amount
 
+    def get_sum_segment(self, segment):
+        current_year = func.EXTRACT("year", func.CURRENT_DATE())
+        total_amount = (
+            DimaBase.query.with_entities(func.sum(DimaBase.dcp_cost))
+            .filter(
+                func.EXTRACT("year", DimaBase.financing_date) == current_year,
+                func.EXTRACT("month", DimaBase.financing_date) <= self.period,
+                DimaBase.client_segment_type == segment,
+            )
+            .scalar()
+        )
+        return total_amount
+
+    def segments_dict(self):
+        return {
+            "small_business": format_number(
+                self.with_out_nds(
+                    self.million(
+                        self.get_sum_segment(
+                            "Малый бизнес (численность персонала до 100 чел.; годовая выручка до 800 млн. руб.)"
+                        )
+                    )
+                )
+            ),
+            "middle_business": format_number(
+                self.with_out_nds(
+                    self.million(
+                        self.get_sum_segment(
+                            "Средний бизнес (численность персонала 100-250 чел; выручка от 800 млн. до 2 млрд руб.)"
+                        )
+                    )
+                )
+            ),
+            "large_business": format_number(
+                self.with_out_nds(
+                    self.million(
+                        self.get_sum_segment(
+                            "Крупный бизнес (численность персонала более 250 чел; выручка более 2 млрд руб.)"
+                        )
+                    )
+                )
+            ),
+            "government": format_number(
+                self.with_out_nds(
+                    self.million(
+                        self.get_sum_segment(
+                            "Госучреждения (ФГУП,  МУП,  органы федер. и местной власти и др.)"
+                        )
+                    )
+                )
+            ),
+            "physical": format_number(
+                self.with_out_nds(
+                    self.million(self.get_sum_segment("Физические лица (но не ИП)"))
+                )
+            ),
+        }
+
     def get_count_current_period(self):
         current_year = func.EXTRACT("year", func.CURRENT_DATE())
         if isinstance(self.period, int):
@@ -82,4 +140,22 @@ class ExpertRa:
             "sum_new_business": format_number(
                 self.million(self.get_sum_indicator(DimaBase.contract_amount))
             ),
+            "new_agreements_count": self.get_count_current_period(),
+            "financed_value": format_number(
+                self.million(
+                    self.get_sum_indicator(DimaBase.credit_sum)
+                    + self.get_sum_indicator(DimaBase.co_finance)
+                    + self.get_sum_indicator(DimaBase.advance)
+                )
+            ),
+            "advance": format_number(
+                self.million(self.get_sum_indicator(DimaBase.advance))
+            ),
+            "co_finance": format_number(
+                self.million(self.get_sum_indicator(DimaBase.co_finance))
+            ),
+            "credit_sum": format_number(
+                self.million(self.get_sum_indicator(DimaBase.credit_sum))
+            ),
+            "client_segment": self.segments_dict(),
         }
