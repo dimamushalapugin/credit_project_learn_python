8fe0c2a173b779e96e9ce161cec5a97939bd4e8a
БадриевА <zzdl4@bk.ru>
2024-03-12 12:25:44 +0300

    added new banks and contracts

--- webapp/payment/create_banks_and_contracts.py
+++ webapp/payment/create_banks_and_contracts.py
@@ -6,44 +6,98 @@ with app.app_context():
     banks = [
         Bank(bank_name="ПАО «АК БАРС» БАНК"),
         Bank(bank_name="АО «АЛЬФА-БАНК»"),
-        Bank(bank_name="АО «КРЕДИТ ЕВРОПА БАНК (РОССИЯ)»"),
         Bank(bank_name="ПАО АКБ «МЕТАЛЛИНВЕСТБАНК»"),
         Bank(bank_name="ПАО «МОСКОВСКИЙ КРЕДИТНЫЙ БАНК»"),
         Bank(bank_name="АО «ПЕРВОУРАЛЬСКБАНК»"),
-        Bank(bank_name="АО «СМП БАНК»"),
+        Bank(bank_name="АО «СМП БАНК» (ПСБ)"),
         Bank(bank_name="АО «СОЛИД БАНК»"),
         Bank(bank_name="АО КБ «УРАЛ ФД»"),
-        Bank(bank_name="СОБСТВЕННЫЕ СРЕДСТВА")
+        Bank(bank_name="ПАО «СОВКОМБАНК»"),
+        Bank(bank_name="ПАО «СБЕРБАНК»"),
+        Bank(bank_name="АО «ИНВЕСТТОРГБАНК»"),
+        Bank(bank_name="СОБСТВЕННЫЕ СРЕДСТВА"),
     ]
 
     contracts = [
-        CreditContract(credit_contract_name="№ 4502/3/2022/5942 от 22.12.2022", bank=banks[0]),
-        CreditContract(credit_contract_name="№ 4502/3/2019/2182 от 13.06.2019", bank=banks[0]),
-        CreditContract(credit_contract_name="№ 4502/3/2019/4312 от 08.11.2019", bank=banks[0]),
-        CreditContract(credit_contract_name="№ 4502/3/2021/1129 от 06.05.2021", bank=banks[0]),
-        CreditContract(credit_contract_name="№ 4502/3/2021/2736 от 11.11.2021", bank=banks[0]),
+        CreditContract(
+            credit_contract_name="№ 4502/3/2022/5942 от 22.12.2022", bank=banks[0]
+        ),
+        CreditContract(
+            credit_contract_name="№ 4502/3/2019/2182 от 13.06.2019", bank=banks[0]
+        ),
+        CreditContract(
+            credit_contract_name="№ 4502/3/2019/4312 от 08.11.2019", bank=banks[0]
+        ),
+        CreditContract(
+            credit_contract_name="№ 4502/3/2021/1129 от 06.05.2021", bank=banks[0]
+        ),
+        CreditContract(
+            credit_contract_name="№ 4502/3/2021/2736 от 11.11.2021", bank=banks[0]
+        ),
+        CreditContract(
+            credit_contract_name="№ 4502/3/2023/2901 от 18.09.2023", bank=banks[0]
+        ),
         CreditContract(credit_contract_name="№ 060Y2L от 26.07.2022", bank=banks[1]),
-        CreditContract(credit_contract_name="№ 000001007603 от 01.12.2021", bank=banks[2]),
-        CreditContract(credit_contract_name="№ 8365-К от 26.03.2021", bank=banks[3]),
-        CreditContract(credit_contract_name="№ 8871-К от 05.10.2021", bank=banks[3]),
-        CreditContract(credit_contract_name="№ 9365-К от 06.07.2022", bank=banks[3]),
-        CreditContract(credit_contract_name="№ 9847-К от 27.02.2023", bank=banks[3]),
-        CreditContract(credit_contract_name="№ 0114/23 от 02.03.2023", bank=banks[4]),
-        CreditContract(credit_contract_name="№ 0683/21 от 12.10.2021", bank=banks[4]),
-        CreditContract(credit_contract_name="№ 0909/20 от 15.10.2020", bank=banks[4]),
-        CreditContract(credit_contract_name="№ 0277/21 от 30.04.2021", bank=banks[4]),
-        CreditContract(credit_contract_name="№ ПУБ 130/2022-КД/ЮЛ от 16.05.2022", bank=banks[5]),
-        CreditContract(credit_contract_name="№ ПУБ 220/2022-КД/ЮЛ от 27.06.2022", bank=banks[5]),
-        CreditContract(credit_contract_name="№ ПУБ 227/2022-КД/ЮЛ от 28.06.2022", bank=banks[5]),
-        CreditContract(credit_contract_name="№ ПУБ 777-КД/ЮЛ от 23.09.2021", bank=banks[5]),
-        CreditContract(credit_contract_name="№ ПУБ 842-КД/ЮЛ от 26.11.2021", bank=banks[5]),
-        CreditContract(credit_contract_name="№ 1400100274.082022КЛ от 04.10.2022", bank=banks[6]),
-        CreditContract(credit_contract_name="№ 0504-2021-2010 от 23.08.2021", bank=banks[7]),
-        CreditContract(credit_contract_name="№ 0504-2022-2006 от 18.04.2022", bank=banks[7]),
-        CreditContract(credit_contract_name="№ 0504-2022-2011 от 25.11.2022", bank=banks[7]),
-        CreditContract(credit_contract_name="№ Ю-3994-КЛВ от 08.02.2022", bank=banks[8]),
-        CreditContract(credit_contract_name="№ Ю-4043-КЛВ от 08.07.2022", bank=banks[8]),
-        CreditContract(credit_contract_name="№ Ю-4179-КЛВ от 31.03.2023", bank=banks[8])
+        CreditContract(credit_contract_name="№ 8365-К от 26.03.2021", bank=banks[2]),
+        CreditContract(credit_contract_name="№ 8871-К от 05.10.2021", bank=banks[2]),
+        CreditContract(credit_contract_name="№ 9365-К от 06.07.2022", bank=banks[2]),
+        CreditContract(credit_contract_name="№ 9847-К от 27.02.2023", bank=banks[2]),
+        CreditContract(credit_contract_name="№ 10222-К от 03.10.2023", bank=banks[2]),
+        CreditContract(credit_contract_name="№ 0114/23 от 02.03.2023", bank=banks[3]),
+        CreditContract(credit_contract_name="№ 0683/21 от 12.10.2021", bank=banks[3]),
+        CreditContract(credit_contract_name="№ 0909/20 от 15.10.2020", bank=banks[3]),
+        CreditContract(credit_contract_name="№ 0277/21 от 30.04.2021", bank=banks[3]),
+        CreditContract(
+            credit_contract_name="№ ПУБ 220/2022-КД/ЮЛ от 27.06.2022", bank=banks[4]
+        ),
+        CreditContract(
+            credit_contract_name="№ ПУБ 227/2022-КД/ЮЛ от 28.06.2022", bank=banks[4]
+        ),
+        CreditContract(
+            credit_contract_name="№ ПУБ 777-КД/ЮЛ от 23.09.2021", bank=banks[4]
+        ),
+        CreditContract(
+            credit_contract_name="№ ПУБ 842-КД/ЮЛ от 26.11.2021", bank=banks[4]
+        ),
+        CreditContract(
+            credit_contract_name="№ ПУБ 225/2023-КД/ЮЛ от 31.05.2023", bank=banks[4]
+        ),
+        CreditContract(
+            credit_contract_name="№ ПУБ 58/2024-КД/ЮЛ от 28.02.2024", bank=banks[4]
+        ),
+        CreditContract(
+            credit_contract_name="№ ПУБ 59/2024-КД/ЮЛ от 28.02.2024", bank=banks[4]
+        ),
+        CreditContract(
+            credit_contract_name="№ ПУБ 348/2023-КД/ЮЛ от 20.10.2023", bank=banks[4]
+        ),
+        CreditContract(
+            credit_contract_name="№ ПУБ 360/2023-КД/ЮЛ от 20.10.2023", bank=banks[4]
+        ),
+        CreditContract(
+            credit_contract_name="№ 1400100274.082022КЛ от 04.10.2022", bank=banks[5]
+        ),
+        CreditContract(
+            credit_contract_name="№ 0504-2021-2010 от 23.08.2021", bank=banks[6]
+        ),
+        CreditContract(
+            credit_contract_name="№ 0504-2022-2011 от 25.11.2022", bank=banks[6]
+        ),
+        CreditContract(
+            credit_contract_name="№ Ю-3994-КЛВ от 08.02.2022", bank=banks[7]
+        ),
+        CreditContract(
+            credit_contract_name="№ Ю-4043-КЛВ от 08.07.2022", bank=banks[7]
+        ),
+        CreditContract(
+            credit_contract_name="№ Ю-4179-КЛВ от 31.03.2023", bank=banks[7]
+        ),
+        CreditContract(
+            credit_contract_name="№ 4518/81-РКЛ/23 от 10.11.2023", bank=banks[8]
+        ),
+        CreditContract(
+            credit_contract_name="№ 23/КЛВ-09 от 15.08.2023", bank=banks[10]
+        ),
     ]
 
     db.session.add_all(banks)
--- webapp/static/js/first-page.js
+++ webapp/static/js/first-page.js
@@ -5,6 +5,7 @@ function updateCreditContracts() {
                     contractSelect.innerHTML = "";
 
                     if (investorSelect.value === "bank1") {
+                        contractSelect.innerHTML += '<option value="№ 4502/3/2023/2901 от 18.09.2023">№ 4502/3/2023/2901 от 18.09.2023</option>';
                         contractSelect.innerHTML += '<option value="№ 4502/3/2022/5942 от 22.12.2022">№ 4502/3/2022/5942 от 22.12.2022</option>';
                         contractSelect.innerHTML += '<option value="№ 4502/3/2019/2182 от 13.06.2019">№ 4502/3/2019/2182 от 13.06.2019</option>';
                         contractSelect.innerHTML += '<option value="№ 4502/3/2019/4312 от 08.11.2019">№ 4502/3/2019/4312 от 08.11.2019</option>';
@@ -14,14 +15,12 @@ function updateCreditContracts() {
                     else if (investorSelect.value === "bank2") {
                         contractSelect.innerHTML += '<option value="№ 060Y2L от 26.07.2022">№ 060Y2L от 26.07.2022</option>';
                     }
-                     else if (investorSelect.value === "bank3") {
-                        contractSelect.innerHTML += '<option value="№ 000001007603 от 01.12.2021">№ 000001007603 от 01.12.2021</option>';
-                    }
                     else if (investorSelect.value === "bank4") {
                         contractSelect.innerHTML += '<option value="№ 8365-К от 26.03.2021">№ 8365-К от 26.03.2021</option>';
                         contractSelect.innerHTML += '<option value="№ 8871-К от 05.10.2021">№ 8871-К от 05.10.2021</option>';
                         contractSelect.innerHTML += '<option value="№ 9365-К от 06.07.2022">№ 9365-К от 06.07.2022</option>';
                         contractSelect.innerHTML += '<option value="№ 9847-К от 27.02.2023">№ 9847-К от 27.02.2023</option>';
+                        contractSelect.innerHTML += '<option value="№ 10222-К от 03.10.2023">№ 10222-К от 03.10.2023</option>';
                     }
                     else if (investorSelect.value === "bank5") {
                         contractSelect.innerHTML += '<option value="№ 0114/23 от 02.03.2023">№ 0114/23 от 02.03.2023</option>';
@@ -30,18 +29,21 @@ function updateCreditContracts() {
                         contractSelect.innerHTML += '<option value="№ 0277/21 от 30.04.2021">№ 0277/21 от 30.04.2021</option>';
                     }
                     else if (investorSelect.value === "bank6") {
-                        contractSelect.innerHTML += '<option value="№ ПУБ 130/2022-КД/ЮЛ от 16.05.2022">№ ПУБ 130/2022-КД/ЮЛ от 16.05.2022</option>';
                         contractSelect.innerHTML += '<option value="№ ПУБ 220/2022-КД/ЮЛ от 27.06.2022">№ ПУБ 220/2022-КД/ЮЛ от 27.06.2022</option>';
                         contractSelect.innerHTML += '<option value="№ ПУБ 227/2022-КД/ЮЛ от 28.06.2022">№ ПУБ 227/2022-КД/ЮЛ от 28.06.2022</option>';
                         contractSelect.innerHTML += '<option value="№ ПУБ 777-КД/ЮЛ от 23.09.2021">№ ПУБ 777-КД/ЮЛ от 23.09.2021</option>';
                         contractSelect.innerHTML += '<option value="№ ПУБ 842-КД/ЮЛ от 26.11.2021">№ ПУБ 842-КД/ЮЛ от 26.11.2021</option>';
+                        contractSelect.innerHTML += '<option value="№ ПУБ 225/2023-КД/ЮЛ от 31.05.2023">№ ПУБ 225/2023-КД/ЮЛ от 31.05.2023</option>';
+                        contractSelect.innerHTML += '<option value="№ ПУБ 58/2024-КД/ЮЛ от 28.02.2024">№ ПУБ 58/2024-КД/ЮЛ от 28.02.2024</option>';
+                        contractSelect.innerHTML += '<option value="№ ПУБ 59/2024-КД/ЮЛ от 28.02.2024">№ ПУБ 59/2024-КД/ЮЛ от 28.02.2024</option>';
+                        contractSelect.innerHTML += '<option value="№ ПУБ 348/2023-КД/ЮЛ от 20.10.2023">№ ПУБ 348/2023-КД/ЮЛ от 20.10.2023</option>';
+                        contractSelect.innerHTML += '<option value="№ ПУБ 360/2023-КД/ЮЛ от 20.10.2023">№ ПУБ 360/2023-КД/ЮЛ от 20.10.2023</option>';
                     }
                     else if (investorSelect.value === "bank7") {
                         contractSelect.innerHTML += '<option value="№ 1400100274.082022КЛ от 04.10.2022">№ 1400100274.082022КЛ от 04.10.2022</option>';
                     }
                     else if (investorSelect.value === "bank8") {
                         contractSelect.innerHTML += '<option value="№ 0504-2021-2010 от 23.08.2021">№ 0504-2021-2010 от 23.08.2021</option>';
-                        contractSelect.innerHTML += '<option value="№ 0504-2022-2006 от 18.04.2022">№ 0504-2022-2006 от 18.04.2022</option>';
                         contractSelect.innerHTML += '<option value="№ 0504-2022-2011 от 25.11.2022">№ 0504-2022-2011 от 25.11.2022</option>';
                     }
                     else if (investorSelect.value === "bank9") {
